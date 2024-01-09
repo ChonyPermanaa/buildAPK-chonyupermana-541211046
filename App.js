594886922461bf07997f447b5a25f26ed91a281e
chonypermanaa <superchony27@gmail.com>
2024-01-09 14:34:32 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
-export default function App() {
+const PortfolioScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>My Portfolio</Text>
+       <Image source={ require("./assets/chony1.png")} style={{ width: 125, height: 125}}></Image>
+
+      <View style={styles.portfolioItem}>
+        <Text style={styles.portfolioTitle}>Project 1</Text>
+        <Text>Ini adalah desain dari tugas kelompok kk4, ditugas ini saya dan kelompok saya membuat aplikasi CallDifit, yaitu aplikasi untuk menghitung calori harian dan program diet yang disertai fitur panduan olahraga dan membeli makan yang sehat </Text>
+        <Image source={ require("./assets/proyek1.png")} style={{ width: 125, height: 125}}></Image>
+      </View>
+
+      <View style={styles.portfolioItem}>
+        <Text style={styles.portfolioTitle}>Project 2</Text>
+        <Text>Ini adalah desain aplikasi ecomerce yang saya buat saat tugas dp4 kelas 10.</Text>
+        <Image source={ require("./assets/chony1.png")} style={{ width: 125, height: 125}}></Image>
+      </View>
+
+      <View style={styles.portfolioItem}>
+        <Text style={styles.portfolioTitle}>Project 3</Text>
+        <Text>Di projek ini saya membuat sebuah desain web dengan html css, web ini adalah web Baqery, saya membuat ini karena pada saat itu saya sering pergi ke Queen Baqery .</Text>
+        <Image source={ require("./assets/home.png")} style={{ width: 125, height: 125}}></Image>
+      </View>
+
+      <Button title="Contact Me :)" onPress={() => {}} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  portfolioItem: {
+    marginBottom: 20,
+  },
+  portfolioTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
+
+export default PortfolioScreen;
